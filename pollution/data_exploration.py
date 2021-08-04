3e6ce8c1f7ee26317b0f147c8e9c8eb419c413e4
Paula Leoni <plbeck@web.de>
2021-08-04 17:31:14 +0200

    some improvements

--- pollution/data_exploration.py
+++ pollution/data_exploration.py
@@ -5,48 +5,159 @@ this file is used for data exploration
 from pathlib import Path
 import numpy as np
 import pandas as pd
-
+import geopandas as gpd
+from shapely.geometry import Point
 
 # Path
 wd=Path.cwd()
 
-# load data
+#*#########################
+#! LOAD DATA
+#*#########################
+
 data = pd.read_csv(wd.parent/'data'/'pollution'/'env_data_0726.csv')
 location = pd.read_csv(wd.parent/'data'/'pollution'/'Indialocationlist.csv')
+grid = gpd.read_file(wd.parent/'data'/'grid'/'gridnwq.shp')
 
+delhi = {'city':'delhi', 'geometry':Point(77.23149, 28.65195)}
+# source: https://www.geodatos.net/en/coordinates/india/delhi
+delhi = gpd.GeoDataFrame(delhi, index=[0], crs='EPSG:4326')
 
+# map of India
+map = gpd.read_file(wd.parent/'data'/'shp'/'sh819zz8121.shp')
+
+#*#########################
+#! Stations Information
+#*#########################
+
+# make timearray to datetimeindex datatype
 data['timearray'] = pd.to_datetime(data['timearray'])
 
 data.describe()
 
+# list of all stations
 stations = data.serialno.drop_duplicates().tolist()
 # nearly 700 stations
 len(stations) 
 
+# initialize df with stations as rows
 stations = pd.DataFrame(stations, columns=['station'])
 
-# evaluate first and last reading
+# evaluate first and last reading, add to df
 stations['first_reading'] = stations.apply(lambda row: data.loc[data['serialno'] == row.station, 'timearray'].min(), axis = 1)
 stations['last_reading'] = stations.apply(lambda row: data.loc[data['serialno'] == row.station, 'timearray'].max(), axis = 1)
-stations['time_coverage'] = stations.apply(lambda row: row.last_reading -row.first_reading, axis = 1)
+stations['time_coverage'] = stations.last_reading - stations.first_reading
 
 # add dummies for pollution available
 sensor = data['sensorName'].drop_duplicates().tolist()
-
 for s in sensor:
     stations[s] = stations.apply(lambda row: True in data.loc[data['serialno'] == row.station, 'sensorName'].str.contains(s).tolist(), axis = 1)
 
 # percent of missings for each stations in avrangearray
-# missing = 0
 stations['missing_perc'] = stations.apply(lambda row: data.loc[(data['serialno'] == row.station) & (data['avrangearray'] == 0), 'avrangearray'].count() / data.loc[data['serialno'] == row.station, 'avrangearray'].count(), axis=1)
 # multiply by 100
 stations['missing_perc'] = stations['missing_perc'] * 100
 
+''' assumes that 0 means missing'''
+''' check code again'''
+
 np.mean(stations.missing_perc) # 4% missing
 
+#*#########################
+#! geo dimension
+#*#########################
 
 # merge with Indialocationlist to get geo dimension
 stations = stations.merge(location, left_on = 'station', right_on = 'serialNo').drop('serialNo', axis = 1)
 
-# write to csv 
+# add column geometry
+stations['geometry'] = stations.apply(lambda row: Point(row.lon, row.lat), axis=1)
+
+#*#########################
+#! extract delhi and add grid
+#*#########################
+
+# extract polygon of delhi in map
+point_delhi = delhi['geometry'][0]
+in_delhi = map.apply(lambda row: point_delhi.within(row.geometry), axis=1)
+map_delhi = map[in_delhi].reset_index(drop=True)
+
+# stations inside polygon of delhi
+stations_delhi = stations.apply(lambda row: row.geometry.within(map_delhi.geometry[0]), axis=1)
+stations_delhi = stations[stations_delhi]
+
+np.mean(stations_delhi.missing_perc) # 1%
+
+# extract centroid of each grid
+grid['center'] = grid.apply(lambda row: row.geometry.centroid,axis=1)
+
+# assign gridcell to stations in dehli
+
+# initialize dictionary
+dict = {}
+for stat in stations_delhi.station:
+    for cell in grid.grid_id:
+        # check if stat in cell
+        point = stations_delhi.loc[stations_delhi.station == stat, 'geometry'].reset_index(drop=True)[0]
+        poly = grid.loc[grid.grid_id == cell,'geometry'].reset_index(drop=True)[0]
+        bool = point.within(poly)
+        if bool == True:
+            # dict with station id as key and values polygon and grid id
+            dict[stat] = (poly,cell)
+
+# add to stations_delhi
+stations_delhi['grid_id'] = stations_delhi.apply(lambda row: dict[row.loc['station']][1], axis=1)
+stations_delhi['grid_geometry'] = stations_delhi.apply(lambda row: dict[row.loc['station']][0], axis=1)
+
+
+#*#########################
+#! GRID as cross-section
+#*#########################
+
+# add stations column to grid
+dict = {}
+for cell in grid.grid_id:
+    list = []
+    for stat in stations_delhi.station:
+        point = stations_delhi.loc[stations_delhi.station == stat, 'geometry'].reset_index(drop=True)[0]
+        poly = grid.loc[grid.grid_id == cell,'geometry'].reset_index(drop=True)[0]
+        bool = point.within(poly)
+        if bool == True:
+            list.append(stat)
+    dict[cell] = list        
+
+grid['stations'] = grid.apply(lambda row: dict[row.grid_id], axis=1)
+
+### add values for sensor based on stations in each gridcell
+''' first only make simple average, later do weighting'''
+# initialize empty columns
+for s in sensor:
+    grid[f'{s}_value'] = 0
+
+#gr = 417
+for gr in grid.grid_id:
+    # extract list of stations in grid cell
+    stat = grid.loc[grid.grid_id == gr,'stations'].reset_index(drop=True)[0]
+    # if stat is not empty
+    if len(stat) > 0:
+        # for each sensor calculate the average of stations in grid
+        for s in sensor:
+            avg = 0
+            #s = sensor[0]
+            #st = stat[0]
+            for st in stat:
+                x = data.loc[(data.serialno == st) & (data.sensorName == s),'avrangearray']
+                # average across time dimension of each station
+                avg += np.mean(x)
+            # average of stations    
+            avg = avg / len(stat)
+            grid.loc[grid.grid_id == gr,f'{s}_value'] = avg      
+
+
+#*#########################
+#! EXPORT to csv
+#*#########################
+
 stations.to_csv(wd.parent/'data'/'data_transformed'/'stations_w_pol_loc.csv')
+stations_delhi.to_csv(wd.parent/'data'/'data_transformed'/'stations_delhi.csv')
+grid.to_csv(wd.parent/'data'/'data_transformed'/'grid_w_stat.csv')
--- pollution/maps.py
+++ pollution/maps.py
@@ -16,49 +16,111 @@ wd=Path.cwd()
 # load data
 map = gpd.read_file(wd.parent/'data'/'shp'/'sh819zz8121.shp')
 stations = pd.read_csv(wd.parent/'data'/'data_transformed'/'stations_w_pol_loc.csv')
+stations_delhi = pd.read_csv(wd.parent/'data'/'data_transformed'/'stations_delhi.csv')
+grid = pd.read_csv(wd.parent/'data'/'data_transformed'/'grid_w_stat.csv')
 
-# prepare data
-stations = stations.drop('Unnamed: 0', axis=1)   
-stations['geometry'] = stations.apply(lambda row: Point(row.lon, row.lat), axis=1)
-
-#some stations are not in India, drop those
-stations = gpd.GeoDataFrame(stations)
-for row in stations.index:
-    if stations.loc[row,'geometry'].x < 50:
-        stations = stations.drop(row) 
-    elif stations.loc[row,'geometry'].x > 100:
-        stations = stations.drop(row)      
-stations = stations.reset_index(drop=True)
-
-# add delhi
 delhi = {'city':'delhi', 'geometry':Point(77.23149, 28.65195)}
 # source: https://www.geodatos.net/en/coordinates/india/delhi
 delhi = gpd.GeoDataFrame(delhi, index=[0], crs='EPSG:4326')
 
 
-fig, ax = plt.subplots(figsize=(12,12))
-map.plot(color='white', edgecolor='grey', ax=ax)
-stations.plot(color='darkred',markersize=8, ax=ax, label='stations')
-delhi.plot(color='green', ax=ax, label='delhi')
-plt.axis('off')
-plt.legend()
-plt.show()
+#*#########################
+#! DATA PREP
+#*#########################
+def prep_data(df, col):
+    '''
+    CSVs created contain an Unnamed: 0 column and columns containing geometries are imported as strings. This function drop the former and transforms the latter.
+    
+    *df=df to be transformed
+    *col=list of column(s) containing geometries
+    '''
+    if 'Unnamed: 0' in df.columns: 
+        df=df.drop('Unnamed: 0', axis=1)    
+    for c in col: 
+        df[c]=df[c].apply(wkt.loads)
+    
+    return df
 
-# focus on delhi
+stations = prep_data(stations, ['geometry'])
+stations_delhi = prep_data(stations_delhi, ['geometry', 'grid_geometry'])
+grid = prep_data(grid, ['geometry'])
 
-# keep only polygon of delhi
+# make a geo df
+stations = gpd.GeoDataFrame(stations)
+stations_delhi = gpd.GeoDataFrame(stations_delhi)
+grid = gpd.GeoDataFrame(grid)
+
+
+#some stations are not in India, drop those
+stations_clear = stations
+for row in stations_clear.index:
+    if stations_clear.loc[row,'geometry'].x < 50:
+        stations_clear = stations_clear.drop(row) 
+    elif stations_clear.loc[row,'geometry'].x > 100:
+        stations_clear = stations_clear.drop(row)      
+stations_clear = stations_clear.reset_index(drop=True)
+
+# 202 dropped
+len(stations) - len(stations_clear) 
+
+# extract unclear points
+x = [x for x in stations.station.tolist() if x not in stations_clear.station.tolist()]
+test = stations[stations.apply(lambda row: row.station in x, axis =1)]
+test.stateName.unique()
+test.countryName.unique()
+
+# extract polygon of delhi in map
 point_delhi = delhi['geometry'][0]
 in_delhi = map.apply(lambda row: point_delhi.within(row.geometry), axis=1)
 map_delhi = map[in_delhi].reset_index(drop=True)
 
-# stations inside polygon
-stations_delhi = stations.apply(lambda row: row.geometry.within(map_delhi.geometry[0]), axis=1)
-stations_delhi = stations[stations_delhi]
-# 171 stations
 
+#*#########################
+#! PLOTS
+#*#########################
+
+# all stations
+fig, ax = plt.subplots(figsize=(12,12))
+map.plot(color='white', edgecolor='grey', ax=ax)
+stations.plot(color='darkred',markersize=8, ax=ax, label='stations')
+plt.title('India with all stations')
+plt.text(108,80,f'{len(stations)} stations')
+plt.axis('off')
+plt.legend()
+plt.show()
+fig.savefig('visualizations/India_stat_all.png')
+
+# stations in India
+fig, ax = plt.subplots(figsize=(12,12))
+map.plot(color='white', edgecolor='grey', ax=ax)
+stations_clear.plot(color='darkred',markersize=8, ax=ax, label='stations')
+delhi.plot(color='green', ax=ax, label='delhi')
+plt.title('India')
+plt.text(94.5,36,f'{len(stations_clear)} stations')
+plt.axis('off')
+plt.legend()
+plt.show()
+fig.savefig('visualizations/India_stat.png')
+
+# stations in delhi
 fig, ax = plt.subplots(figsize=(12,12))
 map_delhi.plot(color='white', edgecolor='grey', ax=ax)
+stations_delhi.plot(color='darkred',markersize=10, ax=ax, label='stations')
+plt.title('Delhi')
+plt.text(77.31,28.85,f'{len(stations_delhi)} stations')
+plt.axis('off')
+plt.legend()
+plt.show()
+fig.savefig('visualizations/Delhi_stat.png')
+
+## add grid
+fig, ax = plt.subplots(figsize=(12,12))
+c = grid['PM25_value']
+map_delhi.plot(color='white', edgecolor='black', ax=ax)
+grid.plot(edgecolor='grey', color='white', ax=ax, label='grid', cmap = 'RdPu')
+cbar = plt.colorbar()
 stations_delhi.plot(color='darkred',markersize=10, ax=ax, label='stations')
 plt.axis('off')
 plt.legend()
 plt.show()
+fig.savefig('visualizations/Delhi_stat_grid.png')
